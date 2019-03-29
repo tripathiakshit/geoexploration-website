c290f657e3c68512212e1adeaacf25b4b658657e
Akshit Tripathi <tripathisoft@gmail.com>
2019-03-28 23:12:57 -0400

    Updated HTML files and added WebStorm defaults
    - Added new CSS routes to all page heads
    - Added mobile navigation to all pages
    - Added details to about, contact, gallery, index, projects, services and technologies pages.
    - Removed config.rb file

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'GeoExploration-cache';
 let urlsToCache = [
     './',
-    './stylesheets/screen.css',
-    './stylesheets/fontawesome/css/fontawesome.css',
-    './stylesheets/fontawesome/css/all.css',
-    './script/main.js'
+    './sass/screen.css'
+    // './script/main.js'
 ];
 
 self.addEventListener('install', function (event) {
@@ -18,10 +16,10 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(function (response) {
+            .then(response => {
                 // Cache hit - return response
                 if (response) {
                     return response;
@@ -29,7 +27,7 @@ self.addEventListener('fetch', function (event) {
                 let fetchRequest = event.request.clone();
 
                 return fetch(fetchRequest).then(
-                    function (response) {
+                    response => {
                         // Check if we received a valid response
                         if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
@@ -37,7 +35,7 @@ self.addEventListener('fetch', function (event) {
                         let responseToCache = response.clone();
 
                         caches.open(CACHE_NAME)
-                            .then(function (cache) {
+                            .then(cache => {
                                 cache.put(event.request, responseToCache);
                             });
 
